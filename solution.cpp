1264285fa1d2c5bd5a1df7e73e759c8e8aea45d1
filbert333 <126647519+filbert333@users.noreply.github.com>
2023-03-03 22:58:10 +0800

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -27,5 +27,22 @@ int main () {
 
 //Solution 2
 int main() {
+    int n;
+    char current;
+    char next;
     
+    std:cin >> n;
+    
+    for (int i = 0; i < n; i++){
+       std: cin >> current;
+       if ( current == " ")
+       {
+           std:cin >> next;
+           if (next = "f")
+           {
+               std::cout << "No" << std::endl;
+           }
+       }
+     std::cout << "Yes" << std::endl;
+    }
 }
