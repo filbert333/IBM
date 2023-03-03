fa82613a71e8fc9d1f94c67331d377dd287e9bae
filbert333 <126647519+filbert333@users.noreply.github.com>
2023-03-03 23:00:47 +0800

    Update and rename solution.cpp to Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+//Solution 1
+int main () {
+    int n;
+    
+    std::cin >> n;
+    
+    int available[n];
+    
+    for (int i = 0; i < n; i++) {
+        std::cin >> available[i];
+    }
+    
+    int m;
+    
+    std:cin >> m;
+    
+    int request[m];
+    
+    for (int i = 0; i < m; i++) {
+        std::cin >> available[i];
+    }
+    
+    
+}
+
