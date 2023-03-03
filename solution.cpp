4cfb971e5a4c74896be9323372396443033b5572
filbert333 <126647519+filbert333@users.noreply.github.com>
2023-03-03 22:33:19 +0800

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 
+//Solution 1
 int main () {
+    int n;
+    
+    std::cin >> n;
+    
+    int available[n];
+    
+    for (int i = 0; i < n; i++) {
+        std::cin >> available[i];
+    }
+    
+    int m;
+    
+    std:cin >> m;
+    
+    int request[m];
+    
+    for (int i = 0; i < m; i++) {
+        std::cin >> available[i];
+    }
+    
+    
+}
 
+//Solution 2
+int main() {
     
 }
